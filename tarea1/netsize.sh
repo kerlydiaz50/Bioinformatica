61ff46f9b2881f97a43cc40a8cfeb71c9af2ce20
kerlydiaz50 <95938386+kerlydiaz50@users.noreply.github.com>
2021-12-11 23:35:08 -0500

    Update netsize.sh

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,2 +1,10 @@
 Numero de filas: wc -l n10.txt
+
+Para las filas se utiliza wc -l archivo (en este caso n10.txt)
+-l significa línea total
+
 Numero de columnas: head -n1 n10.txt | grep -o " " | wc -l
+
+Se utiliza head -n1 para tomar la primera línea del archivo (en este caso n10.txt). 
+Se usa grep -o para contar todos los espacios y sacar cada espacio encontrado en una nueva línea. 
+Se utiliza wc -l para contar el número de líneas.
